7cdf904bab0bd4d4b95ace138ff1c8ab24b68d55
Douglas Morais <mr.douglasmorais23@gmail.com>
2021-08-02 22:11:32 -0300

    steps validations refec

--- src/view/page/Home/index.js
+++ src/view/page/Home/index.js
@@ -1,13 +1,22 @@
 let Home = {
   render: async () => {
+    let texto = "Click me!";
     let view = `
       <div>
         <h1>Home</h1>
+        <h2>Hello!!!</h2>
+        <button id="test">${texto}</button>
       </div>
     `
     return view
   },
-  after_render: async () => {}
+  after_render: async () => {
+    let button = document.getElementById('test');
+    button.addEventListener('click', () => {
+      alert('Tudo certo')
+    })
+
+  }
 }
 
 export default Home;
